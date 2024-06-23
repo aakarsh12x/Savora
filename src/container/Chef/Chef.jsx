b319fd09231dcb260860072c9d2c9d2f5e8ac6db
Aakarsh Singh <aakarshshrey12@gmail.com>
2024-06-24 02:53:27 +0530

    Your commit message

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -16,15 +16,15 @@ const Chef = () => (
       <div className="app__chef-content">
         <div className="app__chef-content_quote">
           <img src={images.quote} alt="quote_image" />
-          <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .</p>
+          <p className="p__opensans"></p>
         </div>
-        <p className="p__opensans"> auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc. </p>
+        <p className="p__opensans"> At Savora, we believe that dining should be an experience that nourishes the body, delights the senses, and fosters community. Our commitment to quality is unwavering; we source the freshest, locally-grown ingredients to create dishes that are not only delicious but also sustainable.  </p>
       </div>
 
       <div className="app__chef-sign">
-        <p>Kevin Luo</p>
+        <p>Karan Ahuja</p>
         <p className="p__opensans">Chef & Founder</p>
-        <img src={images.sign} alt="sign_image" />
+        
       </div>
     </div>
   </div>
